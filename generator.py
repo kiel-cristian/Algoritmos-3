13fe5ae0295de0ae09d82d118a52321c9c3b692c
Cristian <kiel@acid.cl>
2012-06-23 13:37:32 -0400

    generator con test

--- generator.py
+++ generator.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from random import randint
+from printlist import print_list as pl
 #Esta clase representa un generador de instancias(arreglos de tamaño n) de las dos formas requeridas
 #de la tarea, con distribucion uniforme, y de forma aleatoria en el dominio.
+
 class InputGenerator:
 	#range: rango de valores del dominio
 	#n: numero de elementos del arreglo
@@ -9,9 +14,12 @@ class InputGenerator:
 		self.__n = n
 		#self.__input
 
+	def setN(self,n):
+		self.__n = n
+
 	#dist: rango de distribución uniforme de elementos sucesivos de la instancia
 	def uniform_gen(self,dist = [1,16]):
-		rand_num = randint(self.range[0],self.range[1])
+		rand_num = randint(self.__range[0],self.__range[1])
 		previous = rand_num
 		inputset = [rand_num]
 
@@ -31,3 +39,21 @@ class InputGenerator:
 		inputset = []
 		for i in range(0,self.__n):
 			inputset.append(randint(self.__range[0],self.__range[1]))
+		return inputset
+
+	def test(self,dist=[1,16]):
+		n = self.__n
+
+		while n<= 2**20:
+			uniform = self.uniform_gen(dist)
+			random  = self.random_gen()
+			pl("uniform",uniform)
+			pl("random",random)
+
+			n = n*2
+			self.setN(n)
+
+n = 2**10
+ig = InputGenerator(n)
+ig.test([1,16])
+
---
+++ printlist.py
@@ -0,0 +1,2 @@
+def print_list(name,l):
+	print str(name) +" " + str(len(l)) + " : " + str(l)
